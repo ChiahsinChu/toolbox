ed1513c69a69e8fc0b084692735043b35c33e412
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2023-04-14 15:18:46 +0200

    modify default fontsize in legend; modify default red and blue color with XMU style

--- toolbox/plot/style.py
+++ toolbox/plot/style.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+from matplotlib import colors
 from pathlib import Path
 
 MODULE_DIR = Path(__file__).resolve().parent
+
 
 def use_style(style_name):
     fname = str(MODULE_DIR / ("mplstyle/%s.mplstyle" % style_name))
@@ -9,4 +11,12 @@ def use_style(style_name):
     try:
         plt.style.use(fname)
     except:
-        print("Warning: no style %s is found. Use matplotlib default style." % style_name)
+        print("Warning: no style %s is found. Use matplotlib default style." %
+              style_name)
+    """
+    Set colors:
+    https://stackoverflow.com/questions/68664116/is-there-a-way-to-change-the-color-names-color-in-mplstyle-file
+    """
+    color_map = colors.get_named_colors_mapping()
+    color_map["red"] = "#D7422A"
+    color_map["blue"] = "#003C88"
