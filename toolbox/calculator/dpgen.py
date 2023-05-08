00e5bb42d202372019bd0726714376300a936816
Jia-Xin Zhu <xmuchiahsin@gmail.com>
2023-05-08 12:01:18 +0800

    add method to collect npy data from dpgen fp task

--- toolbox/calculator/dpgen.py
+++ toolbox/calculator/dpgen.py
@@ -1,13 +1,19 @@
-from toolbox.utils import *
-from toolbox.utils.utils import save_dict
+import dpdata
+import shutil
+
 from toolbox.io.cp2k import Cp2kInput
+from toolbox.utils import *
+from toolbox.utils.utils import save_dict, safe_makedirs
 
 from .template import machine, params
 
 
 class FPTask:
-    def __init__(self, configs) -> None:
 
+    def __init__(self, work_dir=".") -> None:
+        self.work_dir = work_dir
+
+    def run(self, configs, **kwargs):
         if isinstance(configs, str):
             self.configs = io.read(configs, index=":")
         elif isinstance(configs, list):
@@ -17,7 +23,6 @@ class FPTask:
         else:
             raise AttributeError("Unknown type of configs: %s" % type(configs))
 
-    def run(self, dst, **kwargs):
         dip_cor = kwargs.pop("dip_cor", False)
         for ii, atoms in enumerate(self.configs):
             cp2k_inp = Cp2kInput(atoms,
@@ -26,21 +31,44 @@ class FPTask:
                                  totden=True,
                                  dip_cor=dip_cor)
             cp2k_inp.write(os.path.join(
-                dst, "iter.000000/02.fp/task.000.%06d" % ii),
+                self.work_dir, "iter.000000/02.fp/task.000.%06d" % ii),
                            save_dict=True)
 
-        self._setup(dst, **kwargs)
+        self._setup(**kwargs)
 
-    def _setup(self, dname, **kwargs):
+    def collect(self, type_map, out_dir="deepmd"):
+        safe_makedirs(out_dir)
+
+        ms = dpdata.MultiSystems.from_dir(dir_name=os.path.join(
+                self.work_dir, "iter.000000/02.fp"), file_name="output", fmt="cp2k/output")
+        ms.to_deepmd_npy("tmp_dpdata")
+
+        fname_type_map = glob.glob("tmp_dpdata/**/type_map.raw", recursive=True)[0]
+        _type_map = np.loadtxt(fname_type_map)
+        dname_dpdata = os.path.dirname(fname_type_map)
+        _atype = np.loadtxt(os.path.join(dname_dpdata, "type.raw"), dtype=np.int32)
+
+        # npy data
+        shutil.move(os.path.join(dname_dpdata, "set.000"), os.path.join(out_dir, "set.000"))
+        # type_map.raw
+        np.savetxt(type_map, os.path.join(out_dir, "type_map.raw"), fmt="%s")
+        # type.raw
+        atype = [type_map.index(_type_map[ii]) for ii in _atype]
+        np.savetxt(atype, os.path.join(out_dir, "type.raw"), fmt="%d")
+        
+        os.removedirs("tmp_dpdata")
+
+
+    def _setup(self, **kwargs):
         # record.dpgen
         a = np.zeros((7, 2))
         a[:, 1] = np.arange(7)
-        np.savetxt(os.path.join(dname, "record.dpgen"), fmt="%d")
+        np.savetxt(os.path.join(self.work_dir, "record.dpgen"), fmt="%d")
         # params.json
-        save_dict(params, os.path.join(dname, "params.json"))
+        save_dict(params, os.path.join(self.work_dir, "params.json"))
         # machine.json
         self._set_queue(machine, **kwargs)
-        save_dict(machine, os.path.join(dname, "machine.json"))
+        save_dict(machine, os.path.join(self.work_dir, "machine.json"))
 
     @staticmethod
     def _set_queue(machine, queue="c52-medium"):
