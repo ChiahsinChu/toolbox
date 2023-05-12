aefd8af47cd51ab730fedf99f7257e233ddf6233
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2023-05-12 14:11:49 +0800

    update installtion setup

--- setup.py
+++ setup.py
@@ -9,4 +9,7 @@ setup(
     #find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
     # all packages required for this package is in src
     #package_dir = {'':'zjxpack'},
-    include_package_data=True)
+    include_package_data=True,
+    install_requires=[
+        "monty", "regex", "ase", "scipy", "scikit-learn", "h5py"
+    ])
