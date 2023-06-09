12aa22398aee559003ddb662ae8e43e1b5f5f7cf
Jia-Xin Zhu <xmuchiahsin@gmail.com>
2023-06-09 11:26:06 +0800

    minor update

--- toolbox/plot/figure.py
+++ toolbox/plot/figure.py
@@ -6,8 +6,17 @@ class Figure:
     def __init__(self, **kwargs) -> None:
         self.fig, self.ax = plt.subplots(**kwargs)
 
-    def setup(self, x, y, **kwargs):
+    def setup(self, x, y, xlim=None, ylim=None, **kwargs):
         self.ax.plot(x, y, **kwargs)
+        if xlim is None:
+            xlim = (np.min(x), np.max(x))
+        if ylim is None:
+            ylim = (np.min(y) - (np.max(y) - np.min(y)) * 0.1, 
+                    np.max(y) + (np.max(y) - np.min(y)) * 0.1)
+        self.xlim = xlim
+        self.ylim = ylim
+        self.ax.set_xlim(xlim)
+        self.ax.set_ylim(ylim)
 
     def set_labels(self, kw, xlabel=None, ylabel=None, **kwargs):
         try:
@@ -24,37 +33,25 @@ class FullCellFigure(Figure):
         super().__init__(**kwargs)
 
     def setup(self, x, y, xlim=None, ylim=None, z_surfs=None, **kwargs):
-        if xlim is None:
-            xlim = (np.min(x), np.max(x))
-        if ylim is None:
-            ylim = (np.min(y), np.max(y))
-        self.ax.set_xlim(xlim)
-        self.ax.set_ylim(ylim)
+        super().setup(x, y, xlim, ylim, **kwargs)
 
-        super().setup(x, y, **kwargs)
         ax = self.ax
         ax.axvline(x=z_surfs[0])
         ax.axvline(x=z_surfs[1])
         ax.axvline(x=(z_surfs[0] + z_surfs[1]) / 2, ls="--")
-        ax.fill_between(x, ylim[0], ylim[1], where=x <= z_surfs[0], facecolor='gray', alpha=.5)
-        ax.fill_between(x, ylim[0], ylim[1], where=x >= z_surfs[1], facecolor='gray', alpha=.5)
+        ax.fill_between(x, self.ylim[0], self.ylim[1], where=x <= z_surfs[0], facecolor='gray', alpha=.5)
+        ax.fill_between(x, self.ylim[0], self.ylim[1], where=x >= z_surfs[1], facecolor='gray', alpha=.5)
 
 class HalfCellFigure(Figure):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
-    def setup(self, x, y, z_surf=None, **kwargs):
-        if xlim is None:
-            xlim = (np.min(x), np.max(x))
-        if ylim is None:
-            ylim = (np.min(y), np.max(y))
-        self.ax.set_xlim(xlim)
-        self.ax.set_ylim(ylim)
-
+    def setup(self, x, y, xlim=None, ylim=None, z_surf=None, **kwargs):
         super().setup(x, y, xlim, ylim, **kwargs)
+        
         ax = self.ax
         ax.axvline(x=z_surf)
-        ax.fill_between(x, ylim[0], ylim[1], where=x < z_surf, facecolor='gray', alpha=.5)
+        ax.fill_between(x, self.ylim[0], self.ylim[1], where=x < z_surf, facecolor='gray', alpha=.5)
         
 
 label_dict = {
@@ -64,5 +61,6 @@ label_dict = {
     "elec_inveps": [r"z [A]", r"$\epsilon_{elec,\perp}^{-1}$"],
     "elec_eps": [r"z [A]", r"$\epsilon_{elec,\perp}$"],
     "hartree": [r"z [A]", r"$V_H$ [eV]"],
-    "rho": [r"z [A]", r"$\rho$"]
+    "rho": [r"z [A]", r"$\rho$"],
+    "polarization": [r"z [A]", r"P [eA$^{-2}$]"]
 }
