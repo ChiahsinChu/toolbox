56c80a7e79c6bdebd2bce2eede66afedd161121a
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2023-07-20 15:56:06 +0800

    add attr stats_dict in FDMTest

--- toolbox/plot/stats.py
+++ toolbox/plot/stats.py
@@ -11,18 +11,18 @@ class FDMTest:
         self.fig, self.axs = plt.subplots(nrows=2, figsize=[4, 6], sharex = "all", **kwargs)
 
     def setup(self, x, y, dydx):
-        stats_dict = {}
+        self.stats_dict = {}
         ax = self.axs[0]
         ax.scatter(x, dydx, color="blue", label="original data")
         fit_output = stats.linregress(x, dydx)
         ax.plot(x, x * fit_output.slope + fit_output.intercept, "--", color="red", label="fitted line")
-        stats_dict["slope"] = fit_output.slope
-        stats_dict["intercept"] = fit_output.intercept
-        stats_dict["rvalue"] = fit_output.rvalue
+        self.stats_dict["slope"] = fit_output.slope
+        self.stats_dict["intercept"] = fit_output.intercept
+        self.stats_dict["rvalue"] = fit_output.rvalue
 
         ax = self.axs[1]
         ref_data_diff = np.diff(y, axis=0).reshape(-1)
         test_data_diff = 0.5 * np.diff(x, axis=0) * (dydx[1:] + dydx[:-1])
         ax.scatter(x[1:], ref_data_diff, color="blue", label="original data")
         ax.plot(x[1:], test_data_diff, "--", color="red", label="fitted line")
-        stats_dict["mean error"] = np.mean(ref_data_diff - test_data_diff)
+        self.stats_dict["mean error"] = np.mean(ref_data_diff - test_data_diff)
