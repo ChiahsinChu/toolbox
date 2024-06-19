910a866a8681377066efac6cd4f2f4c374bf20e5
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2024-06-19 18:08:40 +0800

    update DMFFPMECalculator

--- toolbox/calculator/coulomb.py
+++ toolbox/calculator/coulomb.py
@@ -13,6 +13,22 @@ A3 = 1.421413741
 A4 = -1.453152027
 A5 = 1.061405429
 
+try:
+    import torch
+except ImportError:
+    import warnings
+
+    warnings.warn("torch not found, DMFFPMECalculator cannot be used.")
+try:
+    from dp_dmff.dmff.recip import Ck_1, generate_pme_recip
+    from dp_dmff.dmff.pme import energy_pme, setup_ewald_parameters
+    from dp_dmff.dmff.constants import ENERGY_COEFF
+    from dp_dmff.dmff.settings import DEVICE
+except ImportError:
+    import warnings
+
+    warnings.warn("dp_dmff not found, DMFFPMECalculator cannot be used.")
+
 
 def coul(qi, qj, rij):
     """
@@ -103,3 +119,87 @@ class CoulLongCalculator:
             forces[sel_ids] -= delta_x * fpair.reshape(-1, 1)
             energy += np.sum(prefactor * erfc)
         return energy, forces
+
+
+class DMFFPMECalculator:
+    def __init__(self, cutoff=6.0) -> None:
+        self.cutoff = cutoff
+
+    def calculate(self, atoms, ethresh=1e-6) -> float:
+        positions = atoms.get_positions()
+        box = atoms.get_cell()
+        charges = atoms.get_initial_charges()
+        pairs = calculate_pairs(atoms, self.cutoff)
+
+        box = torch.tensor(box, dtype=torch.double, requires_grad=False, device=DEVICE)
+        positions = torch.tensor(
+            positions, dtype=torch.double, requires_grad=True, device=DEVICE
+        )
+        charges = torch.tensor(
+            charges, dtype=torch.double, requires_grad=False, device=DEVICE
+        )
+        pairs = torch.tensor(
+            pairs, dtype=torch.int32, requires_grad=False, device=DEVICE
+        )
+
+        mscales = torch.tensor([0.0, 0.0, 1.0, 1.0, 1.0, 1.0], device=DEVICE)
+        kappa, K = self.setup_ewald(box, ethresh)
+        K1, K2, K3 = K
+        pme_recip_fn = generate_pme_recip(
+            Ck_fn=Ck_1,
+            kappa=kappa,
+            gamma=False,
+            pme_order=6,
+            K1=K1,
+            K2=K2,
+            K3=K3,
+            lmax=0,
+        )
+        charges = torch.reshape(charges, (-1, 1))
+        energy = energy_pme(
+            positions,
+            box,
+            pairs,
+            charges,
+            None,
+            None,
+            None,
+            mscales,
+            None,
+            None,
+            None,
+            pme_recip_fn,
+            kappa,
+            K1,
+            K2,
+            K3,
+            0,
+            False,
+            True,
+        )[0]
+        # from kJ/mol to eV/particle
+        return (energy * ENERGY_COEFF).item()
+
+    def setup_ewald(self, box, ethresh):
+        kappa, K1, K2, K3 = setup_ewald_parameters(
+            torch.tensor(self.cutoff), torch.tensor(ethresh), box, 0.01, "openmm"
+        )
+        K = (K1, K2, K3)
+        return kappa, K
+
+
+def calculate_pairs(atoms, rcut):
+    # calculate pairs
+    cellpar = atoms.cell.cellpar()
+    positions = atoms.get_positions()
+
+    pairs = []
+    dist_mat = distance_array(positions, positions, box=cellpar)
+    for ii, dist_vec in enumerate(dist_mat):
+        mask = dist_vec < rcut
+        jjs = np.where(mask)[0]
+        mask = jjs > ii
+        jjs = jjs[mask]
+        for jj in jjs:
+            pairs.append([ii, jj, 0])
+    return pairs
