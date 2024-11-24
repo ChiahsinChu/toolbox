0d88a2a123713e43be9877402a1df5278993659e
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2024-11-24 11:03:30 +0800

    add `set_energy_and_forces` for ase.Atoms

--- toolbox/io/dpdata.py
+++ toolbox/io/dpdata.py
@@ -1,35 +1,53 @@
-# SPDX-License-Identifier: LGPL-3.0-or-later
-import glob
-import os
-from typing import List
-
 import numpy as np
-from tqdm import tqdm
+
+from ase import Atoms
+from ase.calculators.singlepoint import SinglePointCalculator
 
 
-def wannier_v2_to_v3(dname, sel_type: List[str]):
-    fnames = glob.glob(os.path.join(dname, "**/atomic_dipole.npy"), recursive=True)
-    fnames.sort()
-    for fname in tqdm(fnames):
-        dname = os.path.dirname(fname)
-        # print(dname)
-        _type_map = np.loadtxt(os.path.join(dname, "../type_map.raw"), dtype=str)
-        atype = np.loadtxt(os.path.join(dname, "../type.raw"), dtype=int)
-        symbols = _type_map[atype]
-        sel_ids = []
-        for t in sel_type:
-            sel_ids.extend(np.where(symbols == t)[0].tolist())
-        old_data = np.load(fname)
-        n_frames = old_data.shape[0]
-        new_data = np.zeros_like(np.load(os.path.join(dname, "coord.npy"))).reshape(
-            [n_frames, -1, 3]
-        )
-        # np.copyto(new_data[:, sel_ids], old_data.reshape(n_frames, -1, 3))
-        new_data[:, sel_ids] = old_data.reshape(n_frames, -1, 3)
-        os.rename(
-            os.path.join(dname, "atomic_dipole.npy"),
-            os.path.join(dname, "v2_atomic_dipole.npy"),
-        )
-        np.save(
-            os.path.join(dname, "atomic_dipole.npy"), new_data.reshape([n_frames, -1])
-        )
+def set_energy_and_forces(atoms: Atoms, energy: float, forces: np.ndarray):
+    """
+    Set energy and forces to atoms object.
+    Make atoms.get_potential_energy() and atoms.get_forces() return the given energy and forces.
+
+    Parameters
+    ----------
+    atoms : ase.Atoms
+        The atoms object.
+    energy : float
+        The energy.
+    forces : np.ndarray
+        The forces.
+
+    """
+    calc = SinglePointCalculator(atoms)
+    atoms.set_calculator(calc)
+    atoms.calc.results["energy"] = energy
+    atoms.calc.results["forces"] = forces
+
+
+# if __name__ == "__main__":
+#     import glob
+
+#     from ase import io
+
+#     from dpdata import LabeledSystem, MultiSystems
+#     from toolbox.utils.unit import AU_TO_EV, AU_TO_EV_EVERY_ANG
+#     from toolbox.io.dpdata import set_energy_and_forces
+
+#     fnames_pos = glob.glob("./**/*-pos-*.xyz", recursive=True)
+#     fnames_frc = glob.glob("./**/*-frc-*.xyz", recursive=True)
+#     assert len(fnames_pos) == len(fnames_frc)
+#     fnames_pos.sort()
+#     fnames_frc.sort()
+
+#     ms = MultiSystems()
+
+#     for fname_pos, fname_frc in zip(fnames_pos, fnames_frc):
+#         traj = io.read(fname_pos, ":")
+#         frc_traj = io.read(fname_frc, ":")
+#         for atoms, atoms_frc in zip(traj, frc_traj):
+#             forces = atoms_frc.get_positions() * AU_TO_EV_EVERY_ANG
+#             set_energy_and_forces(atoms, atoms.info["E"] * AU_TO_EV, forces)
+#             ms.append(LabeledSystem(atoms, fmt="ase/structure"))
+
+#     ms.to_deepmd_npy("deepmd")
