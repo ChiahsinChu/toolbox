86fe9f5f1c268016d690ca4bbe48082204b20979
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2025-02-02 17:29:16 +0800

    add simulation module for deepmd-jax md

---
+++ toolbox/io/dp_jax.py
@@ -0,0 +1,141 @@
+from typing import List, Optional
+
+import time
+import sys
+
+import numpy as np
+from ase import io, Atoms
+
+from deepmd_jax.md import Simulation as _Simulation
+
+
+class TrajDump:
+    def __init__(
+        self,
+        atoms: Atoms,
+        fname: str,
+        interval: int,
+        vel: bool = False,
+        **kwargs,
+    ) -> None:
+        self.fname = fname
+        self.interval = interval
+        self.vel = vel
+        self.atoms = atoms
+
+        self.write_settings = kwargs
+
+    def write(self, positions, cell):
+        self.atoms.set_positions(positions)
+        self.atoms.set_cell(cell)
+        io.write(
+            self.fname,
+            self.atoms,
+            **self.write_settings,
+        )
+
+
+class Simulation(_Simulation):
+    def __init__(
+        self,
+        model_path,
+        box,
+        type_idx,
+        mass,
+        routine,
+        dt,
+        initial_position,
+        log_file: Optional[str] = "deepmd_jax.stdout",
+        **kwargs,
+    ):
+        super().__init__(
+            model_path,
+            box,
+            type_idx,
+            mass,
+            routine,
+            dt,
+            initial_position,
+            **kwargs,
+        )
+        self.log_file = log_file
+        if log_file is not None:
+            # export all stdout to log_file
+            self._stdout = sys.stdout
+            sys.stdout = open(log_file, "w", encoding="utf-8")
+
+    def __del__(self):
+        if self.log_file is not None:
+            sys.stdout.close()
+            sys.stdout = self._stdout
+
+    def _initialize_run(self, steps):
+        """
+        Reset trajectory for each new run;
+        if the simulation has not been run before, include the initial state.
+        Initialize run variables.
+        """
+        print(f"# Running {steps} steps...")
+        self._offset = self.step - int(self._is_initial_state)
+        self._tic_of_this_run = time.time()
+        self._tic_between_report = time.time()
+        self._error_code = 0
+        self._print_report()
+        self._is_initial_state = False
+
+    def run(self, steps, dump_list: List[TrajDump]):
+        """
+        Run the simulation for a number of steps.
+        """
+        self._initialize_run(steps)
+        remaining_steps = steps
+        while remaining_steps > 0:
+            # run the simulation for a jit-compiled chunk of steps
+            next_chunk = min(
+                self.report_interval - self.step % self.report_interval,
+                self._step_chunk_size,
+                remaining_steps,
+            )
+            states = (
+                self._state,
+                self._typed_nbrs if self._static_args["use_neighbor_list"] else None,
+                self._error_code,
+                self._neighbor_update_profile,
+            )
+            states_new, traj = self._multiple_inner_step_fn(states, next_chunk)
+            state_new, typed_nbrs_new, error_code, profile = states_new
+            self._error_code |= error_code
+
+            if self._error_code & 16:
+                print("# Warning: Nan or Inf encountered in simulation. Terminating.")
+                remaining_steps = 0
+
+            # If there is any hard overflow, we have to re-run the chunk
+            if not (
+                self._error_code == 0 or self._error_code == 4 or self._error_code == 16
+            ):
+                self._resolve_error_code()
+                continue
+
+            # If nothing overflows, update the tracked state and record the trajectory
+            self._keep_nbr_or_lattice_up_to_date()
+            self._state = state_new
+            self._typed_nbrs = typed_nbrs_new
+            self._neighbor_update_profile = profile
+            if "NPT" in self._routine:
+                self._current_box = self._state.box
+            pos_traj, vel_traj, box_traj = traj
+            self.step += next_chunk
+            remaining_steps -= next_chunk
+
+            # Report at preset regular intervals
+            if self.step % self.report_interval == 0 or remaining_steps == 0:
+                self._print_report()
+
+            for dump in dump_list:
+                if self.step % dump.interval == 0 or remaining_steps == 0:
+                    cell = np.concatenate([np.array(box_traj[-1]), [90, 90, 90]])
+                    dump.write(pos_traj[-1] if not dump.vel else vel_traj[-1], cell)
+
+        self._print_run_profile(steps, time.time() - self._tic_of_this_run)
+        self._keep_nbr_or_lattice_up_to_date()
